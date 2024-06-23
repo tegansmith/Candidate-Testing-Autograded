614e361f588b6542eb798abc439e5e01c2b6fbc4
Tegan Smith <tsimps200@caledonian.ac.uk>
2024-06-23 14:55:25 -0500

    part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,8 +5,8 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = 'What is your name?: ';
-let correctAnswer = 'enter name';
+let question = 'Who was the first American woman in space? ';
+let correctAnswer = 'Sally Ride';
 let candidateAnswer = 'Your answer: ';
 
 
@@ -16,14 +16,15 @@ let correctAnswers;
 let candidateAnswers;
 
 
-function askForName() {
+function askForName(){
+
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question('What is your name?: ');
+candidateName = input.question('What is your name?: ')
 }
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-candidateAnswer = input.question("Your Answer: ");
+  console.log(questions);
+  candidateAnswers = input.question('Your Answer: ');
 }
 function gradeQuiz(candidateAnswers) {
 
