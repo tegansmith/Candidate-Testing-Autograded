e770307795ccc1c76aff8be2ba12e4536b4b35b3
Tegan Smith <tsimps200@caledonian.ac.uk>
2024-06-23 13:02:15 -0500

    part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = 'What is your name?: ';
+let correctAnswer = 'enter name';
+let candidateAnswer = 'their name';
 
 
 //TODO: Variables for Part 2
@@ -18,15 +18,13 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question('What is your name?: ');
 }
-
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-
+candidateAnswer = input.question("Your Answer: ");
 }
-
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
