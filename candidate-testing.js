fb0bf2ee8267d6a6f732661fec7366dd93bbbf1b
Tegan Smith <tsimps200@caledonian.ac.uk>
2024-06-24 11:00:05 -0500

    Part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,9 @@ let candidateAnswer = 'Your answer: ';
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ['Who was the first American woman in space? ','True or false: 5 kilometer == 5000 meters? ','(5 + 3)/2 * 10 = ? ','Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS? '];
+let correctAnswers; ['Sally Ride','true','40','Trajectory','3'];
+let candidateAnswers;[];
 
 
 function askForName(){
@@ -23,8 +23,8 @@ candidateName = input.question('What is your name?: ')
 }
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  console.log(questions);
-  candidateAnswers = input.question('Your Answer: ');
+  console.log(question);
+  candidateAnswer = input.question('Your Answer: ');
 }
 function gradeQuiz(candidateAnswers) {
 
