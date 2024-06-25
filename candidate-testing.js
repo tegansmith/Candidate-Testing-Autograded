015a56ee8d2ee0989d027eb0d168fdd2127aedbe
Tegan Smith <tsimps200@caledonian.ac.uk>
2024-06-24 20:12:34 -0500

    update

--- candidate-testing.js
+++ candidate-testing.js
@@ -42,7 +42,7 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
    console.log("Hello " + candidateName);
-  askQuestion 
+  askQuestion();
   console.log(question);
   gradeQuiz(this.candidateAnswer);
 }
